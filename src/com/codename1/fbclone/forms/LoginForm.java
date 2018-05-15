59aef5f9840dd9afbfa8e1d3e5d0d96ce14214d8
Shai Almog <shai@codenameone.com>
2018-05-15 08:27:20 +0300

    Improved indentation

--- src/com/codename1/fbclone/forms/LoginForm.java
+++ src/com/codename1/fbclone/forms/LoginForm.java
@@ -19,27 +19,27 @@ import com.codename1.ui.layouts.FlowLayout;
 import com.codename1.util.Callback;
 
 public class LoginForm extends Form {
+
     private Label logo = new Label("\uf308", "IconFont");
-    private TextField user = new TextField("", "Email or Phone", 30, 
-            TextField.EMAILADDR);
-    private TextField password = new TextField("", "Password", 30, 
-            TextField.PASSWORD);
+    private TextField user = new TextField("", "Email or Phone", 30,
+        TextField.EMAILADDR);
+    private TextField password = new TextField("", "Password", 30,
+        TextField.PASSWORD);
 
     private Button login = new Button("Log In");
     private Button signUp = new Button("Sign Up for Facebook");
     private Button needHelp = new Button("Need Help?");
-    
+
     public LoginForm() {
         super(new BorderLayout());
         getToolbar().setUIID("Container");
-        logo.setName("Logo");        
+        logo.setName("Logo");
         Container logoContainer = BorderLayout.centerAbsolute(logo);
         logoContainer.setUIID("LoginTitle");
-        
+
         signUp.addActionListener(e -> UIController.showSignup());
         login.addActionListener(e -> {
-            User u = new User().
-                    password.set(password.getText());
+            User u = new User().password.set(password.getText());
             if(user.getText().contains("@")) {
                 u.email.set(user.getText());
             } else {
@@ -50,52 +50,54 @@ public class LoginForm extends Form {
                 @Override
                 public void onSucess(User value) {
                     d.dispose();
-                    UIController.showMainUI();            
+                    UIController.showMainUI();
                 }
 
                 @Override
-                public void onError(Object sender, Throwable err, int errorCode, String errorMessage) {
+                public void onError(Object sender, Throwable err,
+                    int errorCode, String errorMessage) {
                     d.dispose();
                     ToastBar.showErrorMessage("Login Failed");
                 }
             });
         });
-        
+
         add(NORTH, logoContainer);
         if(!isTablet()) {
-            BorderLayout bl = ((BorderLayout)getLayout());
+            BorderLayout bl = ((BorderLayout) getLayout());
             bl.defineLandscapeSwap(NORTH, EAST);
         }
-                
+
         if(getUIManager().isThemeConstant("ComponentGroupBool", false)) {
-            Container content = 
-                    BorderLayout.centerAbsolute(
-                        BoxLayout.encloseY(
-                            ComponentGroup.enclose(
-                                    user,
-                                    password),
-                            login));
+            Container content =
+                BorderLayout.centerAbsolute(
+                    BoxLayout.encloseY(
+                        ComponentGroup.enclose(
+                            user,
+                            password),
+                        login));
             content.setUIID("PaddedContainer");
             add(CENTER, content);
-            
+
             login.setUIID("BlueButtonOnBlueBackground");
             setUIID("SplashForm");
             signUp.setUIID("WhiteLinkButton");
             needHelp.setUIID("WhiteLinkButton");
-            content.add(SOUTH, 
-                    FlowLayout.encloseCenter(
-                            signUp, needHelp));
+            content.add(SOUTH,
+                FlowLayout.encloseCenter(
+                    signUp, needHelp));
         } else {
             login.setUIID("BlueButton");
-            Button forgotPassword = new Button("Forgot Password", "BlueText");
+            Button forgotPassword =
+                new Button("Forgot Password", "BlueText");
             signUp.setUIID("GreenButton");
             signUp.setText("Create new Facebook Account");
             Container cnt = BoxLayout.encloseY(
-                    user,
-                    password,
-                    login,
-                    forgotPassword,
-                    signUp);
+                user,
+                password,
+                login,
+                forgotPassword,
+                signUp);
             cnt.setUIID("PaddedContainer");
             cnt.setScrollableY(true);
             add(CENTER, cnt);
@@ -104,5 +106,5 @@ public class LoginForm extends Form {
 
     protected boolean shouldPaintStatusBar() {
         return false;
-    }    
+    }
 }
